b761288bf9c7a1dd858c900496e9aececa614292
Nikolay Masliy <deckards2505@gmail.com>
2022-06-20 20:56:34 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -113,9 +113,37 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  function initLazyLoadVideos() {
+    var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+
+    if ("IntersectionObserver" in window) {
+      var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(video) {
+          if (video.isIntersecting) {
+            for (var source in video.target.children) {
+              var videoSource = video.target.children[source];
+              if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                videoSource.src = videoSource.dataset.src;
+              }
+            }
+  
+            video.target.load();
+            video.target.classList.remove("lazy");
+            lazyVideoObserver.unobserve(video.target);
+          }
+        });
+      });
+  
+      lazyVideos.forEach(function(lazyVideo) {
+        lazyVideoObserver.observe(lazyVideo);
+      });
+    }
+  }
+
   initMenu();
   initScrollToBlock();
   initCounter();
+  initLazyLoadVideos();
   const da = new DynamicAdapt("max");
   da.init();
   AOS.init({
